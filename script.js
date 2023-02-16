ab3cc86ad2a5220909aa2acea69e9ef51089f4e9
murphyed <cmurphyed@gmail.com>
2023-02-16 04:26:12 -0600

    Updated

--- script.js
+++ script.js
@@ -27,10 +27,6 @@ let current = 0;
 let target = 0;
 let ease = .05;
 
-function lerp(start, end, t) {
-  return start * (1 - t) + end * t;
-}
-
 function setTransform(el, transform) {
   el.style.transform = transform;
 }
