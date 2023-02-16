2a97a107248766caa0546a45a8aaf48bde36e1bf
murphyed <cmurphyed@gmail.com>
2023-02-15 20:41:18 -0600

    Functional Update
    Added functionality to showcase how singly linked lists work.

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const lineOne = document.getElementById("lineOne");
 const lineTwo = document.getElementById("lineTwo");
 const button = document.getElementById("add");
 const arrayContainer = document.getElementById("arrayContainer");
-const objectContainer = document.getElementById("objectContainer");
+const linkedContainer = document.getElementById("linkedContainer");
 const fixedContainer = document.querySelector(".container-two");
 const dyContainer = document.querySelector(".container");
-const bitType = Number(document.getElementById("bit-type").value);
-const dataType = document.getElementById("data-type").value;
+const bitType = Number(document.getElementById("bit-type"));
+const dataType = document.getElementById("data-type");
 const userValue = document.getElementById("userValue");
 const arrayBtn = document.getElementById("arrayBtn");
 const objectBtn = document.getElementById("objectBtn");
@@ -41,7 +41,6 @@ function setBorderRadius(el, bordeRadius) {
 function init() {
   containerWidth = fixedContainer.getBoundingClientRect().width * 2;
   document.body.style.height = `${containerWidth - (window.innerWidth - window.innerHeight)}px`;
-
 }
 
 function animate() {
@@ -74,6 +73,8 @@ button.addEventListener("click", () => {
 //View buttons
 arrayBtn.addEventListener("click", () => {
   if (!arrayBtn.classList.contains("activeBtn")) {
+    linkedContainer.classList.add("remove");
+    arrayContainer.classList.remove("remove");
     arrayBtn.classList.add("activeBtn");
     arrayBtn.classList.remove("inactiveBtn");
     objectBtn.classList.add("inactiveBtn");
@@ -82,6 +83,8 @@ arrayBtn.addEventListener("click", () => {
 })
 objectBtn.addEventListener("click", () => {
   if (!objectBtn.classList.contains("activeBtn")) {
+    arrayContainer.classList.add("remove");
+    linkedContainer.classList.remove("remove");
     objectBtn.classList.add("activeBtn");
     objectBtn.classList.remove("inactiveBtn");
     arrayBtn.classList.add("inactiveBtn");
@@ -108,6 +111,16 @@ for (let i = 1; i < 801; i++) {
     slotNumber.innerText = `address: ${numberBinary(i)}`;
   })
   arrayContainer.appendChild(memorySlot);
+
+  const lMemorySlot = document.createElement("div");
+  lMemorySlot.setAttribute("class", "memory");
+  lMemorySlot.setAttribute("id", `slotL${i}`);
+  lMemorySlot.addEventListener("click", () => {
+    slotContainer.classList.remove("hidden");
+    slotNumber.innerText = `address: ${numberBinary(i)}`;
+  })
+
+  linkedContainer.appendChild(lMemorySlot);
 }
 
 //number to binary
@@ -139,24 +152,64 @@ function stringBinary(string) {
   return binaryString
 }
 
-
-//Value retreival
-function valueRetreival(value) {
-  if (value === undefined) {
-    document.querySelector(".decimalValue").innerHTML = "allocated";
-    document.querySelector(".binaryValue").innerHTML = "...";
-  } else {
-    if (isNaN(value)) {
-      const stringV = stringBinary(value);
-      document.querySelector(".decimalValue").innerHTML = value;
+//Coverting value to binary
+function toBinary(passedValue, structure, pointer) {
+  if (structure === "array") {
+    if (isNaN(passedValue)) {
+      const stringV = stringBinary(passedValue);
+      document.querySelector(".decimalValue").innerHTML = passedValue;
       document.querySelector(".binaryValue").innerHTML = stringV;
     } else {
-      const numberV = numberBinary(value);
-      document.querySelector(".decimalValue").innerHTML = value;
+      const numberV = numberBinary(passedValue);
+      document.querySelector(".decimalValue").innerHTML = passedValue;
+      document.querySelector(".binaryValue").innerHTML = numberV;
+    }
+  }
+  if (structure === "linked" && pointer === true) {
+    if (isNaN(passedValue)) {
+      const stringV = stringBinary(passedValue);
+      document.querySelector(".decimalValue").innerHTML = `next value at: ${passedValue}`;
+      document.querySelector(".binaryValue").innerHTML = stringV;
+    } else {
+      const numberV = numberBinary(passedValue);
+      document.querySelector(".decimalValue").innerHTML = `next value at: ${passedValue}`;
+      document.querySelector(".binaryValue").innerHTML = numberV;
+    }
+  } else if (structure === "linked" && pointer === false) {
+    if (isNaN(passedValue)) {
+      const stringV = stringBinary(passedValue);
+      document.querySelector(".decimalValue").innerHTML = passedValue;
+      document.querySelector(".binaryValue").innerHTML = stringV;
+    } else {
+      const numberV = numberBinary(passedValue);
+      document.querySelector(".decimalValue").innerHTML = passedValue;
       document.querySelector(".binaryValue").innerHTML = numberV;
     }
   }
 }
+
+
+//Array value retreival
+function arrayValueRetreival(value, type) {
+  if (value === undefined) {
+    document.querySelector(".decimalValue").innerHTML = "allocated";
+    document.querySelector(".binaryValue").innerHTML = "...";
+  } else {
+    toBinary(value, type);
+  }
+}
+
+//Linked value retreival
+function linkedValueRetreival(value, type, pointer) {
+  if (value === undefined || value === null) {
+    document.querySelector(".decimalValue").innerHTML = "null";
+    document.querySelector(".binaryValue").innerHTML = "...";
+  } else {
+    toBinary(value, type, pointer);
+  }
+}
+
+
 
 
 //ARRAY ALGORITHM
@@ -198,8 +251,6 @@ function randomNumbersArray(amount) {
       slot.classList.add("allocatedMemory");
     }
   }
-
-
   return numbers
 }
 
@@ -225,7 +276,7 @@ function readingValues(numbers) {
   for (let i = 0; i < numbers.length; i++) {
     let slot = document.getElementById(`slot${numbers[i]}`);
     slot.addEventListener("click", () => {
-      valueRetreival(arrayValues[i]);
+      arrayValueRetreival(arrayValues[i], "array");
     })
   }
 }
@@ -239,14 +290,88 @@ function updateAllocatedSlots(valuesArray) {
   slot.classList.add("memoryInUse");
 }
 
+
+
+
+//LINKEDLIST ALGORITHM
+const linkedValues = [];
+
+//Generating a non-repeating random number
+const availableNumbers = Array.from({ length: 799 }, (_, i) => i);
+const linkedPairs = []
+
+function randomNonRepeating() {
+  console.log("pointer");
+  //Generate a random index based on availableNumbers array length
+  let randomIndex = Math.floor(Math.random() * availableNumbers.length);
+  let randomNonRepeating = availableNumbers[randomIndex];
+  console.log(randomNonRepeating);
+  linkedPairs.push([randomNonRepeating, randomNonRepeating + 1]);
+  availableNumbers.splice(randomIndex, 1);
+  availableNumbers.splice(randomIndex - 1, 1);
+}
+
+
+function linkedUI(linkedArray) {
+  randomNonRepeating();
+  for (let i = 0; i < linkedArray.length; i++) {
+    const memorySlot = document.getElementById(`slotL${linkedPairs[i][0]}`)
+    const memorySlotPointer = document.getElementById(`slotL${linkedPairs[i][1]}`);
+    memorySlot.classList.remove("memory");
+    memorySlotPointer.classList.remove("memory");
+    memorySlot.classList.add("memoryInUse");
+    memorySlotPointer.classList.add("allocatedMemory");
+
+    memorySlot.addEventListener("click", () => {
+      linkedValueRetreival(linkedValues[i], "linked", false);
+    })
+    memorySlotPointer.addEventListener("click", () => {
+      if (i === linkedArray.length - 1) {
+        linkedValueRetreival(undefined);
+      } else {
+        linkedValueRetreival(linkedPairs[i + 1][0], "linked", true);
+      }
+    })
+
+    memorySlotPointer.addEventListener("mouseenter", () => {
+      if (i === linkedArray.length - 1) {
+        //Add class
+        return
+      } else {
+        const nextValueAddress = document.getElementById(`slotL${linkedPairs[i + 1][0]}`);
+        nextValueAddress.classList.add("nextValueAddress");
+      }
+    })
+    memorySlotPointer.addEventListener("mouseleave", () => {
+      if (i === linkedArray.length - 1) {
+        //Add class
+        return
+      } else {
+        const nextValueAddress = document.getElementById(`slotL${linkedPairs[i + 1][0]}`);
+        nextValueAddress.classList.remove("nextValueAddress");
+      }
+    })
+
+  }
+}
+
+
 //Getting user input
 function onSubmit() {
   // let bit = bitType;
-  // let data = dataType;
+  let data = dataType.value;
   let value = userValue.value;
-  arrayValues.push(value);
-  checkMemoryStatus();
-  readingValues(numbers);
-  updateAllocatedSlots(arrayValues);
+
+  if (data === "0") {
+    arrayValues.push(value);
+    checkMemoryStatus();
+    readingValues(numbers);
+    updateAllocatedSlots(arrayValues);
+  }
+  if (data === "1") {
+    linkedValues.push(value);
+    linkedUI(linkedValues);
+  }
+
   // return bit, data, value;
 }
